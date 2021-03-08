473cd558df9ac57809611bfb4760565d449ebc63
Denisa N <nova.deni@gmail.com>
2021-03-08 22:23:59 +0100

    callback hell

--- cv3/app.js
+++ cv3/app.js
@@ -1,3 +1,26 @@
 "use strict";
 
+setTimeout(()=>{
+    console.log(1);
+}, 2000);
+console.log(2);
+setTimeout(()=>{
+    console.log(3);
+},3000);
 
+//CALLBACK
+setTimeout(() => {
+    console.log('first');
+    setTimeout(() =>{
+        console.log('second');
+        setTimeout(() =>{
+            console.log('third');
+            setTimeout(() => {
+                console.log('fourth');
+                setTimeout(() => {
+                    console.log('fifth');
+                },3000);
+            },3000);
+        },3000);
+    },3000);
+},3000);
