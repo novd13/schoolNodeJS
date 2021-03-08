8ff134a52b78a5ab5efe48b4feea269ee2df460d
Denisa N <nova.deni@gmail.com>
2021-03-08 19:54:32 +0100

    map reduce

--- cv3/app.js
+++ cv3/app.js
@@ -1,6 +1,13 @@
 "use strict";
 const assert = require("assert");
-assert.fail();
 
-//hello;
+let ar = [10,20,30,40,50,60];
+
+//for (var x of ar){ console.log(x)} //proceduralni
+ar.map(x=>x*3).filter(x=>x>100).reduce((acc,x)=>acc+x); //funkcionalni
+
+//rekurze je bezna ve funkcionalnim (volani sama sebe)
+
+
+
 
