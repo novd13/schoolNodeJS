a2fc4994c53b06783f470b3613e39705cd9fb5e8
Denisa N <nova.deni@gmail.com>
2021-03-08 20:59:31 +0100

    Factorials

--- cv3/app.js
+++ cv3/app.js
@@ -1,13 +1,3 @@
 "use strict";
-const assert = require("assert");
-
-let ar = [10,20,30,40,50,60];
-
-//for (var x of ar){ console.log(x)} //proceduralni
-ar.map(x=>x*3).filter(x=>x>100).reduce((acc,x)=>acc+x); //funkcionalni
-
-//rekurze je bezna ve funkcionalnim (volani sama sebe)
-
-
 
 
