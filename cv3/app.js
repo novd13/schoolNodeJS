4636bff162d02c7605fc71c676f638b17d72648a
Denisa N <nova.deni@gmail.com>
2021-03-12 03:26:19 +0100

    working readFile + html

--- cv3/app.js
+++ cv3/app.js
@@ -3,25 +3,25 @@
 const http = require('http');
 const readFile = require('fs/promises').readFile;
 
-const server = http.createServer((req, res)=>{
-    readFile('client' +(req.url == "/" ? "/index.html" : req.url))
-    .then(f=> {
+const server = http.createServer((req, res) => {
+    readFile('cv3/client' + (req.url == "/" ? "/index.html" : req.url))
+        .then(f => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    fs.readFile('client/index.html').then(f=>res.end(f));
-    res.end(f);
-})
-.catch(e=>{
-    console.error(e);
-    res.statusCode=404;
-    res.end();
-})
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            //   readFile('client/index.html').then(f=>res.end(f));
+            res.end(f);
+        })
+        .catch(e => {
+            console.error(e);
+            res.statusCode = 404;
+            res.end();
+        })
     console.log(req);
 });
 
 
-server.listen(8080, ()=>console.log('Server listening: '+server.listening));
+server.listen(8080, () => console.log('Server listening: ' + server.listening));
 
 /* setTimeout(()=> {
     server.close(()=>console.log('server closed'));
