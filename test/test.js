663b3a6eeb050f8392ed55dd2c01a2bd59525430
novd13 <nova.deni@gmail.com>
2021-04-07 16:07:24 +0200

    test.js change

--- test/test.js
+++ test/test.js
@@ -1,7 +1,23 @@
 import assert from 'assert';
+import { dbHset } from '../myutils.mjs';
+import { dbHgetall } from '../myutils.mjs';
 
 describe('true', ()=>{
     it('should be true', ()=>{
-        assert.strictEqual(true, false);
+        assert.strictEqual(true, true);
+    })
+})
+
+describe('myutils', ()=>{
+    describe('dbHgetall', ()=>{
+        before(async ()=>{
+            await dbHset('random key', 'name', 'testname')
+        });
+        it('should return what previously stored in db', async ()=>{
+            assert.strictEqual(
+            (await dbHgetall('random key')).name,
+            'testname'
+            )
+        })
     })
 })
